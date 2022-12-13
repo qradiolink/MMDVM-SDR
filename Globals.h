05573c7d4dc6b55a0c5525d9034e3f7a4b98ff8f
adrian <adrian@qradiolink.org>
2022-12-13 16:43:59 +0200

    Increase RX ringbuffer size to two slots

--- Globals.h
+++ Globals.h
@@ -124,7 +124,7 @@ const uint8_t  MARK_NONE  = 0x00U;
 const uint16_t RX_BLOCK_SIZE = 2U;
 
 const uint16_t TX_RINGBUFFER_SIZE = 721U;
-const uint16_t RX_RINGBUFFER_SIZE = 960U;
+const uint16_t RX_RINGBUFFER_SIZE = 1442U;
 
 #if defined(STM32F105xC) || defined(__MK20DX256__)
 const uint16_t TX_BUFFER_LEN = 2000U;
